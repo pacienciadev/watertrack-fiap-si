c6b29dedc02b4e10dc06bd08f517fd98d5e9bad0
Filipe <filipealves2621@gmail.com>
2024-06-06 20:36:27 -0300

    Criaçao e desenvolvimento da Dashboard

--- src/components/Cards/Horizontal/index.tsx
+++ src/components/Cards/Horizontal/index.tsx
@@ -3,116 +3,30 @@ import Box from '@mui/material/Box'
 import Card from '@mui/material/Card'
 import CardContent from '@mui/material/CardContent'
 import Typography from '@mui/material/Typography'
-import { Avatar, Button, Chip, IconButton } from '@mui/material'
-import { IoIosHeartEmpty, IoMdPin } from 'react-icons/io'
-import { FaStar } from 'react-icons/fa'
-import { FaRegSquarePlus } from 'react-icons/fa6'
 
 export type HorizontalCardProps = {
   title: string
-  category: string
-  description: string
   image: string
-  distance: string
-  score: string
 }
 
 export default function HorizontalCard({
   title,
-  category,
-  description,
   image,
-  distance,
-  score,
 }: HorizontalCardProps) {
   const theme = useTheme()
 
   return (
-    <Box display="flex" gap={1}>
-      <Avatar
-        alt={title}
-        src={image}
-        variant="rounded"
-        sx={{ width: 56, height: 56 }}
-      />
 
-      <Card sx={{ display: 'flex', flex: 1 }}>
-        <Box sx={{ display: 'flex', flexDirection: 'column', flex: 1 }}>
-          <CardContent sx={{ flex: '1 0 auto' }}>
-            <Box
-              mb={1}
-              display="flex"
-              gap={1}
-              justifyContent="space-between"
-              alignItems="center"
-            >
-              <Chip label={category} size="small" />
+    <Card sx={{ display: 'flex', width: '160px', height: '207px' }}>
+      <Box sx={{ display: 'flex', flexDirection: 'column', flex: 1 }}>
+        <CardContent sx={{ flex: '1 0 auto', alignContent: 'center', alignItems: 'center', textAlign: 'center' }}>
 
-              <IconButton
-                aria-label="add to favorite"
-                size="small"
-                sx={{ pt: 1 }}
-              >
-                <IoIosHeartEmpty />
-              </IconButton>
-            </Box>
-
-            <Typography component="div" variant="h6">
-              {title}
-            </Typography>
-
-            <Typography
-              variant="subtitle2"
-              color="text.secondary"
-              component="div"
-            >
-              {description}
-            </Typography>
-
-            <Box
-              display="flex"
-              justifyContent="space-between"
-              mt={1}
-              alignItems="center"
-            >
-              <Box display="flex" gap={1}>
-                <Chip
-                  icon={
-                    <Box mt={0.25} pl={0.25}>
-                      <IoMdPin size={14} />
-                    </Box>
-                  }
-                  label={distance + ' km'}
-                  size="small"
-                  variant="outlined"
-                />
-
-                <Chip
-                  icon={
-                    <Box mt={0.25} pl={0.25}>
-                      <FaStar size={14} />
-                    </Box>
-                  }
-                  label={score}
-                  size="small"
-                  variant="outlined"
-                />
-              </Box>
-
-              <Box>
-                <Button
-                  variant="text"
-                  size="small"
-                  endIcon={<FaRegSquarePlus />}
-                  sx={{ alignItems: 'start', lineHeight: 1.55 }}
-                >
-                  Detalhes
-                </Button>
-              </Box>
-            </Box>
-          </CardContent>
-        </Box>
-      </Card>
-    </Box>
+          <img src={image} alt="Imagem" style={{ width: '80px', height: '80px', margin: '20px 0 20px 0' }} />
+          <Typography variant="h6" color={'#4C7B00'}>
+            {title}
+          </Typography>
+        </CardContent>
+      </Box>
+    </Card>
   )
 }
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,20 +1,36 @@
-import { Box } from '@mui/material'
+import { Box, Grid } from '@mui/material'
 import HorizontalCard from '../../components/Cards/Horizontal'
 import { MainLayout } from '../../components/Layouts/MainLayout'
+import clima_vector from '../../assets/images/clima-vector.png';
+import marc_vector from '../../assets/images/marc-vetor.png';
+import precip_vector from '../../assets/images/precip-vetor.png';
+import temp_vector from '../../assets/images/previ-vetor.png';
 
 export function Home() {
+
+  const cards = [
+    { title: 'Clima', image: clima_vector },
+    { title: 'Maré', image: marc_vector },
+    { title: 'Precipitação', image: precip_vector },
+    { title: 'Temperatura', image: temp_vector },
+  ]
+
   return (
     <MainLayout>
-      <Box display="flex" flexDirection="column" px={2} gap={2}>
-        <h3>Cards</h3>
-        <HorizontalCard
-          title="Título do card"
-          category="categoria"
-          description="descrição"
-          image="https://picsum.photos/100"
-          distance="1.2"
-          score="4.2"
-        ></HorizontalCard>
+      <Box display="flex" flexDirection="column">
+        <h3>Praia & Zona Costeira</h3>
+        <p>Online</p>
+      </Box>
+      <Box style={{ display: 'flex', width: '100%', alignItems: 'center', margin: '20px' }}>
+        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, m: 3 }}>
+          {
+            cards.map((card, index) => (
+              <Grid item xs={6} sm={3} key={index}>
+                <HorizontalCard title={card.title} image={card.image} />
+              </Grid>
+            ))
+          }
+        </Grid>
       </Box>
     </MainLayout>
   )
